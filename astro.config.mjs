527e6340602add92c2cac02eaaae34b336cee369
NicelyTinned <hael.calapan13@gmail.com>
2026-05-17 19:48:25 +0800

    Set site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://nicelytinned.github.io",
+	base: "/skll",
 	integrations: [
 		starlight({
 			title: 'My Docs',
